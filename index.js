1c0dade9bbc25c6eebdc48df879b90d5f2d65f74
Ndajong Noel <noelmilex20@yahoo.com>
2024-02-17 04:49:14 +0100

    Modified index.js to check the regNum field.

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ document.addEventListener("DOMContentLoaded",function (){
 
         const userName = document.getElementById("userName").value;
         const password = document.getElementById("password").value;
+        const regNum = document.getElementById("regNum").value;
 
-        if(validateInput(userName, password)){
+        if(validateInput(userName, password, regNum)){
             alert("Login Successful!");
         } else{
             alert("Invalid userName, password or registration number. Please try again.");
         }
     });
 
-    function validateInput(userName, password){
-        if (userName.trim() == "" || password.trim() == ""){
+    function validateInput(userName, password, regNum){
+        if (userName.trim() == "" || password.trim() == "" || regNum.trim() == ""){
             return false;
         } else{
             return true;
